4d06b14cdd29192f15cef130988d50ee8f0825b6
Wessl <lovewessman@gmail.com>
2021-09-02 16:38:11 +0200

    Update camera, now smoother zooming & greater control in close zooms

--- Assets/Scripts/CameraControlOverride.cs
+++ Assets/Scripts/CameraControlOverride.cs
@@ -11,9 +11,12 @@ public class CameraControlOverride : MonoBehaviour
     private GameObject player;
 
     private Vector3 panOrigin;
+    static float t = 0;
+    private float orthogCameraSize;
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
+        orthogCameraSize = cmv_Camera.m_Lens.OrthographicSize;
     }
 
     void Update()
@@ -35,10 +38,38 @@ public class CameraControlOverride : MonoBehaviour
             cmv_Camera.transform.position += difference;
         }
 
-        // Scroll wheel zooming
-        var scrollDelta = Input.mouseScrollDelta.y;
-        cmv_Camera.m_Lens.OrthographicSize -= scrollDelta;
-        if (cmv_Camera.m_Lens.OrthographicSize <= 1) cmv_Camera.m_Lens.OrthographicSize = 1;
+        // Scroll wheel zooming with some lerping for smoothness
+        float scrollDelta = Input.mouseScrollDelta.y;
+        if (!scrollDelta.Equals(0f))
+        {
+            // Apply scroll delta, with less effect when near closest possible point to enable greater control in high zoom
+            orthogCameraSize -= Input.mouseScrollDelta.y * orthogCameraSize/(4);
+            // Start timer
+            t = 0 + Time.deltaTime;
+        }
+
+        cmv_Camera.m_Lens.OrthographicSize = Mathf.Lerp(cmv_Camera.m_Lens.OrthographicSize, orthogCameraSize, t); 
+        
+        // Only if timer has been started do we keep incrementing it (always lerp from 0)
+        if (t > 0)
+        {
+            t += Time.deltaTime;
+        }
+        
+        // Since lerping is between 0-1, we can reset once it hits one
+        if (t > 1.0f)
+        {
+            t = 0;
+        }
+
+        // If we try to zoom closer than orthogSize of 1, just say "no thank you" and stay at 1.
+        if (cmv_Camera.m_Lens.OrthographicSize <= 1)
+        {
+            cmv_Camera.m_Lens.OrthographicSize = 1;
+            orthogCameraSize = 1;
+        } 
+        
+        
 
 
 
--- Assets/TODO.cs
+++ Assets/TODO.cs
@@ -31,7 +31,7 @@
 // for the following. just look at official line rider game..........
 
 // smoother camera zooming
-// better UI / UX
+//~ better UI / UX
 // better erasing
 // smoother drawing from the get go
 // ragdoll physics character
